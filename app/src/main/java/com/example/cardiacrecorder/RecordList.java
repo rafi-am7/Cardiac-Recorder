bf7faaad143e1c642455e2ca77e12200d2cca052
abir <abir196600@gmail.com>
2022-06-26 00:33:28 +0600

    updated 'updateRecord' function

--- app/src/main/java/com/example/cardiacrecorder/RecordList.java
+++ app/src/main/java/com/example/cardiacrecorder/RecordList.java
@@ -23,13 +23,7 @@ public class RecordList {
     }
 
     public void updateRecord (Record old_record,String date, String time, int systolic, int diastolic, int heart_rate, String comment) {
-        old_record.setDate(date);
-        old_record.setTime(time);
-        old_record.setSystolic(systolic);
-        old_record.setDiastolic(diastolic);
-        old_record.setHeart_rate(heart_rate);
-        old_record.setComment(comment);
-        //old_record = Record(date,time,systolic,diastolic,heart_rate,comment);
+        old_record =new Record(date,time,systolic,diastolic,heart_rate,comment);
     }
 
     public void fetchListfromDB () {
