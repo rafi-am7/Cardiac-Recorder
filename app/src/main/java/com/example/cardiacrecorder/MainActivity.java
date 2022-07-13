fbc870b59207256654bbef6376f57abc49b28b7b
Rafi Al Mahmud <rafialmahmudc@gmail.com>
2022-07-13 11:10:40 +0600

    main activity database

--- app/src/main/java/com/example/cardiacrecorder/MainActivity.java
+++ app/src/main/java/com/example/cardiacrecorder/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.cardiacrecorder;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.example.cardiacrecorder.adapter.RecordAdapter;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -24,6 +27,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,14 +37,19 @@ RecyclerView recyclerView;
 RecordAdapter recordAdapter;
 LinearLayoutManager linearLayoutManager;
 Record record;
-
+SharedPreferences sharedPreferences;
+SharedPreferences.Editor editor;
+Gson gson;
 ArrayList<Record> recordsArrayList;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recordsArrayList = new ArrayList<>();
+       // recordsArrayList = new ArrayList<>();
+        retrieveData();
         recyclerView = findViewById(R.id.rvRecords);
         linearLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
         recordAdapter = new RecordAdapter(this,R.color.good,R.color.normal,R.color.critical,recordsArrayList);
@@ -48,11 +57,18 @@ ArrayList<Record> recordsArrayList;
         recyclerView.setAdapter(recordAdapter);
         recyclerView.setLayoutManager(linearLayoutManager);
 
-        record = new Record("11-12-21","11:21",126,121,34);
+/*        record = new Record("11-12-21","11:21",126,121,34);
         recordsArrayList.add(record);
         recordsArrayList
                 .add(new Record("01-01-01","11:21",126,121,34));
+        saveData();
+        recordsArrayList
+                .add(new Record("01-01-01","11:21",126,121,34));*/
+
+
+
         recordAdapter.notifyDataSetChanged();
+
 
 
 
@@ -85,6 +101,28 @@ ArrayList<Record> recordsArrayList;
             }
         }*/
         // recordAdapter = new RecordAdapter(this, ContextCompat.getColor(this,R.color.good),ContextCompat.getColor(this,R.color.normal),ContextCompat.getColor(this,R.color.critical),recordList);
+    }
+    private void saveData()
+    {
+        sharedPreferences = getSharedPreferences("shared",MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+        gson = new Gson();
+        String jsonString = gson.toJson(recordsArrayList);
+        editor.putString("record",jsonString);
+        editor.apply();
+    }
+    private void retrieveData()
+    {
+        sharedPreferences = getSharedPreferences("shared",MODE_PRIVATE);
+        gson = new Gson();
+        String jsonString = sharedPreferences.getString("record",null);
+        Type type = new TypeToken<ArrayList<Record>>(){}.getType();
+        recordsArrayList = gson.fromJson(jsonString,type);
+        if(recordsArrayList ==null)
+        {
+            recordsArrayList = new ArrayList<Record>();
+        }
+
     }
 
 
